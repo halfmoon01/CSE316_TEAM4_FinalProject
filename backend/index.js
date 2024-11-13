e488203647c0eba1f1850f480e6be51b6d9e1f68
Hyeseong Bak <hsbak.business@gmail.com>
2024-11-13 18:42:51 +0900

    feat: Add login feature
    I added login feature using cookie. The cookie lasts for a day and it will allow the user to be automatically authentificated when returns to the website, if the user did not log out before leaving.

--- backend/index.js
+++ backend/index.js
@@ -214,6 +214,46 @@ app.post("/signup", (req, res) => {
   });
 });
 
+app.post("/signin", (req, res) => {
+  const { memberId, password } = req.body;
+
+  db.query(
+    "SELECT * FROM members WHERE memberId = ?",
+    [memberId],
+    (err, results) => {
+      if (err) {
+        console.error("Database query error:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      if (results.length === 0) {
+        return res.status(401).json({ error: "Invalid ID or password" });
+      }
+
+      const member = results[0];
+
+      if (member.password !== password) {
+        return res.status(401).json({ error: "Invalid ID or password" }); // 비밀번호 불일치
+      }
+
+      const memberCookie = JSON.stringify({
+        id: member.id,
+      });
+
+      res.cookie("member", memberCookie, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+
+      res.status(200).json({
+        message: "Login successful",
+      });
+    }
+  );
+});
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080.");
 });
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,97 @@
+import { useRef, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "@/css_files/login.css";
+import { checkAuth } from "../AuthTracker";
+import { hashutil } from "../Hashutil.js";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
+  const [memberId, setMemberId] = useState("");
+  const [password, setPassword] = useState("");
+  const { isLoggedIn, setIsLoggedIn, isLoading } = checkAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = "Login Page";
+  }, []);
+
+  const alerted = useRef(false);
+
+  useEffect(() => {
+    if (!isLoading && isLoggedIn && !alerted.current) {
+      alerted.current = true;
+      alert("Already logged in!\nRedirecting to Home Screen.");
+
+      navigate("/HomeScreen");
+    }
+  }, [isLoggedIn]);
+
+  if (isLoggedIn) {
+    return <h1>No Permission!</h1>;
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const hashed = hashutil(memberId, password);
+
+    try {
+      await axios.post(
+        "http://localhost:8080/signin",
+        {
+          memberId,
+          password: hashed, // 전송할 데이터
+        },
+        {
+          withCredentials: true, // 옵션: 쿠키 포함
+        }
+      );
+      alert("Login successful! Welcome!");
+      setIsLoggedIn(true);
+      navigate("/Welcome"); // 로그인 성공 시 대시보드로 이동
+      window.location.reload();
+    } catch (error) {
+      alert("Invalid ID or password");
+    }
+  };
 
   return (
-    <></>
+    <div className="login">
+      <h3>Welcome to CO;Ders Us</h3>
+
+      <form onSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          id="memberId"
+          value={memberId}
+          placeholder="Enter your ID"
+          onChange={(e) => setMemberId(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          id="password"
+          value={password}
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit" className="submit-button">
+          Login
+        </button>
+      </form>
+      <div className="toSignUp">
+        <p>Don't have an account?</p>
+        <Link to="/SignUp">Sign up Now</Link>
+      </div>
+    </div>
   );
 };
 
