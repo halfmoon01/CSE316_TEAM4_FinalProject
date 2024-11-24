27c9e797bb771d1634d57b36d22aaade78819603
Hyeseong Bak <hsbak.business@gmail.com>
2024-11-24 11:17:43 +0900

    feat: Add Welcome Page
    The Welcome Page displays a central message to greet the user by name, making the experience more personal and engaging.
    - Main message: "Welcome, <User Name>" prominently displayed in the center.
    - Responsive design ensures proper scaling across devices.
    - Added placeholder styling for the user name dynamically rendered.

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,7 +1,41 @@
+import { useEffect } from "react";
+import "@/css_files/welcome.css";
+import { checkAuth } from "../AuthTracker";
+import { useNavigate, Link } from "react-router-dom";
+
 const Welcome = () => {
+  useEffect(() => {
+    document.title = "Welcome Page";
+  }, []);
+  const { isLoggedIn, isLoading, name } = checkAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (!isLoggedIn) {
+      alert(
+        "You have to sign in first in order to access the page!\nNavigating to the Login Page..."
+      );
+      navigate("/LogIn");
+    }
+  }, [isLoading, isLoggedIn, navigate]);
+
+  if (!isLoggedIn) {
+    return <h1>No Permission!</h1>;
+  }
+  
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
-    <></>
+    <div className="welcome-container">
+      <h2>Welcome back, {name}</h2>
+      <Link to="/HomeScreen">
+        <p>Click here to go home</p>
+      </Link>
+    </div>
   );
 };
 
