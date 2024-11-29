b45e5650bd38ae70b9bda1bb020d6e2e106bca20
halfmoon01 <onurivit01@gmail.com>
2024-11-29 11:55:13 +0900

    fix: Fix Contact Info Dialog styles
    I fixed contact info dialog colors, background colors, line colors, and desgin to follow overall project design stream

--- frontend/src/dialog/Contact.jsx
+++ frontend/src/dialog/Contact.jsx
@@ -5,37 +5,41 @@ const Contact = ({ isOpen, onClose, selectedExecutive }) => {
   if (!isOpen || !selectedExecutive) return null;
 
   return (
-    <div className="contact-dialog-box" onClick={(e) => e.stopPropagation()}>
-  {/* Close Icon */}
-  <span className="close-icon" onClick={onClose}>
-    &times;
-  </span>
+    <div className="modal" onClick={onClose}>
+      {/* Backdrop */}
+      <div className="backdrop"></div>
 
-  {/* Upper Section */}
-  <div className="upper-section">
-    <img
-      src={selectedExecutive.profileImageUrl || UserIcon}
-      className="contact-image"
-      alt="Profile"
-    />
-    <div className="text-section">
-      <h2 className="contact-name">{selectedExecutive.name}</h2>
-      <h4 className="contact-position">{selectedExecutive.position}</h4>
+      <div className="contact-dialog-box" onClick={(e) => e.stopPropagation()}>
+      {/* Close Icon */}
+      <span className="close-icon" onClick={onClose}>
+        &times;
+      </span>
+
+      {/* Upper Section */}
+      <div className="upper-section">
+        <img
+          src={selectedExecutive.profileImageUrl || UserIcon}
+          className="contact-image"
+          alt="Profile"
+        />
+      <div className="text-section">
+        <h2 className="contact-name">{selectedExecutive.name}</h2>
+        <h4 className="contact-position">{selectedExecutive.position}</h4>
+      </div>
     </div>
+
+    {/* Divider Line */}
+    <hr className="line" />
+
+    {/* Lower Section */}
+    <p className="contact-info">
+      <strong>Phone Number:</strong> {selectedExecutive.phoneNumber}
+    </p>
+    <p className="contact-info">
+      <strong>E-Mail Address:</strong> {selectedExecutive.email}
+    </p>
   </div>
-
-  {/* Divider Line */}
-  <hr className="line" />
-
-  {/* Lower Section */}
-  <p className="contact-info">
-    <strong>Phone Number:</strong> {selectedExecutive.phoneNumber}
-  </p>
-  <p className="contact-info">
-    <strong>E-Mail Address:</strong> {selectedExecutive.email}
-  </p>
 </div>
-
   );
 };
 
